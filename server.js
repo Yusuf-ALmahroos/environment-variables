a6fd08e9b59e17ca5333d2e4f151a66f402fc9d6
Yousif Shukri <yousif4shukri@gmail.com>
2025-04-26 12:25:56 +0300

    added password to env

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('The server is running');
-  console.log(process.env.SECRET_PASSWORD);
 });
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
+  console.log("My Password " + process.env.SECRET_PASSWORD);
 });
 
